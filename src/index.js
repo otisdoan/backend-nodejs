0712e35d620cff92f74e5b91a09301f0e8b8e5c6
LE DOAN HIEU <152629519+OTISDOAN@users.noreply.github.com>
2025-05-25 23:13:26 +0700

    update

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const configViewEngine = require('./configs/viewEngine')
+const webRouter = require('./routes/web')
 
 const port = process.env.PORT;
 
 configViewEngine(app);
 
-app.get('/', (req, res) => {
-  res.render('sample.ejs')
-})
+app.use('/', webRouter);
+
 app.listen(port)
