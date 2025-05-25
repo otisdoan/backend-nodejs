33385a04c024b8f45ad5556e58f9b12825003acf
LE DOAN HIEU <152629519+OTISDOAN@users.noreply.github.com>
2025-05-25 21:37:56 +0700

    update

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+require('dotenv').config();
+
+const port = process.env.PORT;
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.render('sample.ejs')
+})
+app.listen(port)
