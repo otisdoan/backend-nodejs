77ad1f4cde16f9595d11ba13adb902671a358a5f
LE DOAN HIEU <152629519+OTISDOAN@users.noreply.github.com>
2025-05-26 21:04:11 +0700

    update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 const configViewEngine = require('./configs/viewEngine')
 const webRouter = require('./routes/web')
+const mysql = require('mysql2');
 
 const port = process.env.PORT;
 
@@ -11,4 +12,18 @@ configViewEngine(app);
 
 app.use('/', webRouter);
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3307,
+  user: 'root',
+  password: 'Anhzac12345@#!',
+  database: 'otisdoan'
+})
+
+connection.query(
+  'select * from Users',
+  (err, results) => {
+    console.log(results);
+  }
+)
 app.listen(port)
