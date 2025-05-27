6155fd0ba2332cb9e680bf62ea7658c754d7180e
LE DOAN HIEU <152629519+OTISDOAN@users.noreply.github.com>
2025-05-27 21:37:52 +0700

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const path = require('path');
 const app = express();
 const configViewEngine = require('./configs/viewEngine')
 const webRouter = require('./routes/web')
-const connection = require('./configs/database')
+// const connection = require('./configs/database')
+const mysql = require('mysql2');
 
 const port = process.env.PORT;
 
@@ -12,11 +13,53 @@ configViewEngine(app);
 
 app.use('/', webRouter);
 
-connection.query(
-  'select * from Users',
-  (err, results) => {
-    console.log(results);
+app.get('/old', async (req, res) => {
+  var total = 0;
+  for (var i = 1; i < 1000; i++) {
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    })
+
+    await connection.connect(
+      'select * from Users',
+    )
+
+    total++;
   }
-)
+  res.send(`Average: ${total / 1000}`)
+})
+
+app.get('/pool', async (req, res) => {
+  var total = 0;
+  for (var i = 1; i < 1000; i++) {
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      waitForConnections: true,
+      connectionLimit: 1000,
+      queueLimit: 0
+    })
+
+    await connection.connect(
+      'select * from Users',
+    )
+
+    total++;
+  }
+  res.send(`Average: ${total / 1000}`)
+})
+// connection.query(
+//   'select * from Users',
+//   (err, results) => {
+//     console.log(results);
+//   }
+// )
 
 app.listen(port)
