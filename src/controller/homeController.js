b257db2f8e99a60aab735660c1ae7447fd8fa1b7
LE DOAN HIEU <152629519+OTISDOAN@users.noreply.github.com>
2025-05-27 23:18:19 +0700

    update

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,9 +1,20 @@
+const connection = require('../configs/database');
+
 const getHome = (req, res) => {
   res.render('home.ejs')
 }
 
 const addUser = (req, res) => {
-  res.send('Ok')
+  const { email, name, city } = req.body;
+  const query = 'INSERT INTO Users (email, name, city) VALUES (?, ?, ?)';
+  connection.query(
+    query,
+    [email, name, city],
+    (err, results) => {
+      console.log(results);
+      res.send('Ok')
+    }
+  )
 }
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 require('dotenv').config();
 const express = require('express');
-const path = require('path');
 const app = express();
 const configViewEngine = require('./configs/viewEngine')
 const webRouter = require('./routes/web')
-const connection = require('./configs/database')
 
 const port = process.env.PORT;
 
