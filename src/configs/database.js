38982a5dcc0db24ae6bc88befd080e84988f8837
LE DOAN HIEU <152629519+OTISDOAN@users.noreply.github.com>
2025-05-31 21:06:12 +0700

    update

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const connection = mongoose.connect('mongodb://admin:123456@localhost:27017')
-  .then((connect) => console.log('Connected!', connect)).catch((error) => {
-    console.log(error)
-  });
+try {
+  const connection = mongoose.connect('mongodb://admin:123456@localhost:27018')
+  console.log("Connected!")
+  return connection;
+} catch (error) {
+  console.log(error)
+}
 
 console.log('connection', connection);
 
